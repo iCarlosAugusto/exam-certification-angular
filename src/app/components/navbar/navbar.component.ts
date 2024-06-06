72b6d2b906d4aacadf8645a3309b5c3ce6b5f535
iCarlosAugusto <augusto.dev@proton.me>
2024-06-06 12:39:32 -0300

    feat: :sparkles: melhrado navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,11 +3,13 @@ import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
+    CommonModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
@@ -16,5 +18,23 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  isAuthenticated = true;
+  isShowingProfileSettings = false;
+
+  showProfileSettings() {
+    if(this.isShowingProfileSettings === false){
+      console.log("switchProfileSetting");
+      this.isShowingProfileSettings = true;
+    }
+
+  }
+
+  hindeProfileSettings() {
+    if(this.isShowingProfileSettings === true){
+      console.log("Hide!");
+      this.isShowingProfileSettings = false;
+    }
+  }
+
 
 }
