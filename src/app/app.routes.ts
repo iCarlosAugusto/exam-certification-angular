5b86f0711b568314762e0ed92047e02425f4ba77
iCarlosAugusto <augusto.dev@proton.me>
2024-06-05 23:03:20 -0300

    feat: :sparkles: singup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { QuestionsComponent } from './pages/questions/questions.component';
 import { AuthenticationComponent } from './pages/authentication/authentication.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     { path: '',title: "Exam Certification", component: HomeComponent },
     { path: 'questions',title: "Questions", component: QuestionsComponent },
-    { path: 'authentication',title: "Identificação", component: AuthenticationComponent },
+    { path: 'authentication', title: "Identificação", component: AuthenticationComponent },
+    { path: 'signup', title: "Cadastro", component: SignupComponent }
 ];
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
-import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-authentication',
+  selector: 'app-signup',
   standalone: true,
-  imports: [MatInputModule, MatButtonModule, MatDividerModule, RouterLink],
-  templateUrl: './authentication.component.html',
+  imports: [MatInputModule, MatButtonModule, MatDividerModule],
+  templateUrl: './signup.component.html',
 })
-export class AuthenticationComponent {
+export class SignupComponent {
 
 }
