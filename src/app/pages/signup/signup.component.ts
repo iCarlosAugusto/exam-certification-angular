815ddc663d0999fb2ca80ef66ac7f15ff3c63eef
iCarlosAugusto <augusto.dev@proton.me>
2024-06-06 11:46:52 -0300

    refactor: :recycle: navegacao

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatInputModule, MatButtonModule, MatDividerModule],
+  imports: [MatInputModule, MatButtonModule, MatDividerModule, RouterLink],
   templateUrl: './signup.component.html',
 })
 export class SignupComponent {
